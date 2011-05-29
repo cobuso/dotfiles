8cf17bcbfd8cc380c6f8b31192737592bb1dfa76
Tom Cowan <tom@cobuso.co.uk>
2011-05-29 23:17:02 +0100

    Initial commit of dotfiles

---
+++ bash_profile
@@ -0,0 +1,17 @@
+alias flushdns="sudo dscacheutil -flushcache"
+
+alias vim="mvim"
+
+alias vu="vagrant up"
+alias vh="vagrant halt"
+alias vssh="vagrant ssh"
+
+alias g="git"
+alias gs="git status"
+alias gp="git pull"
+alias gpush="git push"
+
+export PATH="~/projects/git-deploy/bin:~/projects/cobuso/scripts:/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/Applications:$PATH"
+
+export RUBYOPT=rubygems
+export COPYFILE_DISABLE=true
---
+++ bootstrap.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+# Bootsteaps my config. Assumes it is being all from the directory where it resides
+#
+if [ ! -f README ]
+then
+  echo "You must execute this script from the root of the dotfiles repo"
+  exit 1
+fi
+
+# Bootstrap the vim config
+ln -s $HOME/dotfiles/vim/vimrc $HOME/.vimrc
+ln -s $HOME/dotfiles/vim $HOME/.vim
+ln -s $HOME/dotfiles/vim/sparkup/vim $HOME/dotfiles/vim/bundle/sparkup
+mkdir -p $HOME/.vim/tmp/backup
+mkdir -p $HOME/.vim/tmp/swap 
+
+# Bash config
+ln -s $HOME/dotfiles/bash_profile $HOME/.bash_profile
+
+# Git
+ln -s $HOME/dotfiles/gitconfig $HOME/.gitconfig 
+
+# Ack
+ln -s $HOME/dotfiles/ackrc $HOME/.ackrc
