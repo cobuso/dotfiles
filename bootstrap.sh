3bb102e8898fb601b57396ccab332e724e8e1c77
Tom Cowan <tom@cobuso.co.uk>
2012-08-02 12:31:36 +0100

    Added zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+# Function to add a symlink. Stolen from mafro (https://github.com/mafrosis/dotfiles)
+function create_sym() {
+  if [ -h "$2" ]; then
+    # if symlink exists, replace it
+    ln -fs "$1" "$2"
+  elif [ -f "$2" ]; then
+    # if a file exists, ask user
+    echo "Do you want to delete the existing config file '$2'? [y/N]"
+    read y
+    if [ y == "y" ]; then
+      # replace existing file
+      ln -fs "$1" "$2"
+    fi
+  else
+    # doesnt exist, so create
+    ln -s "$1" "$2"
+  fi
+}
+
 #
-# Bootsteaps my config. Assumes it is being all from the directory where it resides
+# Bootsteaps my config. Assumes it is being called from the directory where it resides
 #
 if [ ! -f README ]
 then
@@ -9,25 +28,32 @@ then
   exit 1
 fi
 
+PWD=$(pwd)
+
 # Bootstrap the vim config
-ln -s $HOME/dotfiles/vim/vimrc $HOME/.vimrc
-ln -s $HOME/dotfiles/vim $HOME/.vim
-ln -s $HOME/dotfiles/vim/sparkup/vim $HOME/dotfiles/vim/bundle/sparkup
+create_sym $PWD/vim/vimrc $HOME/.vimrc
+create_sym $PWD/vim $HOME/.vim
+create_sym $PWD/vim/sparkup/bin $PWD/vim/bundle/sparkup
 mkdir -p $HOME/.vim/tmp/backup
 mkdir -p $HOME/.vim/tmp/swap 
 mkdir -p $HOME/.vim/tmp/yankring 
 
 # Bash config
-ln -s $HOME/dotfiles/bash_profile $HOME/.bash_profile
+create_sym $PWD/bash_profile $HOME/.bash_profile
+
+# ZSH
+create_sym $PWD/zsh/zshrc $HOME/.zshrc
+create_sym $PWD/zsh/zsh_aliases $HOME/.zsh_aliases
 
 # TMUX
-ln -s $HOME/dotfiles/tmux $HOME/.tmux.conf
+create_sym $PWD/tmux $HOME/.tmux.conf
 
 # Git
-ln -s $HOME/dotfiles/gitconfig $HOME/.gitconfig 
+create_sym $PWD/gitconfig $HOME/.gitconfig
 
 # Ack
+create_sym $PWD/ackrc $HOME/.ackrc
 ln -s $HOME/dotfiles/ackrc $HOME/.ackrc
 
 # OSX Lion sensible defaults
-. $HOME/dotfiles/osx
+. $PWD/osx
