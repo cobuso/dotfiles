2a77c108dcb8c90ecbbc83552a6e4b81b9fd5895
Tom Cowan <tom@cobuso.co.uk>
2017-11-28 11:28:36 +0000

    Updates

--- install.sh
+++ install.sh
@@ -14,18 +14,12 @@ git submodule update --init --recursive
 
 # osx
 if [ "$(uname)" = "Darwin" ]; then
-  #. $PWD/osx/sensible_defaults
   . $PWD/osx/homebrew
 fi
 
 # Vim - config
 create_sym $PWD/vim/vimrc $HOME/.vimrc
-create_sym $PWD/vim/ideavimrc $HOME/.ideavimrc
 create_sym $PWD/vim $HOME/.vim
-
-# Vimperator
-create_sym $PWD/vim/vimperatorrc $HOME/.vimperatorrc
-create_sym $PWD/vim $HOME/.nvim
 
 # Vim - dirs
 mkdir -p $HOME/.vim/tmp/backup
@@ -37,14 +31,8 @@ mkdir -p $HOME/.vim/tmp/yankring
 vim +BundleInstall +qall
 
 # zsh
-create_sym $PWD/prezto $HOME/.zprezto
 create_sym $PWD/zsh/zshrc $HOME/.zshrc
 create_sym $PWD/zsh/zsh_aliases $HOME/.zsh_aliases
-create_sym $PWD/zsh/zlogin $HOME/.zlogin
-create_sym $PWD/zsh/zlogout $HOME/.zlogout
-create_sym $PWD/zsh/zpreztorc $HOME/.zpreztorc
-create_sym $PWD/zsh/zprofile $HOME/.zprofile
-create_sym $PWD/zsh/zshenv $HOME/.zshenv
 
 # tmux
 create_sym $PWD/tmux/tmux.conf $HOME/.tmux.conf
